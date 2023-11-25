3e0616a3cb1ea482c3e5a03ddfba96d3dd06dc44
Benign X <1341398182@qq.com>
2023-11-25 18:36:37 +0800

    support deal array and buffer
    Signed-off-by: Benign X <1341398182@qq.com>

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,6 @@
 #![no_main]
 
 #[no_mangle]
-fn fab(n: i32) -> i32 {
-    if n <= 2 {
-        return 1;
-    }
-    let mut count = 0;
-    let mut a = 1;
-    let mut b = 1;
-    let mut c: i32;
-
-    while count <= n - 2 {
-        c = a + b;
-        a = b;
-        b = c;
-
-        count += 1;
-    }
-
-    a
-}
-
-#[no_mangle]
 fn fake_img() -> usize {
     // Image loading/saving is outside scope of this library
     let width = 10;
