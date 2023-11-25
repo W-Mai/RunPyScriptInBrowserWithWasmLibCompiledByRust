443135e2d7cb4989230e28aa8c1e9ad6626964bf
Benign X <1341398182@qq.com>
2023-11-25 22:02:43 +0800

    support parse png and deal with quantity
    Signed-off-by: Benign X <1341398182@qq.com>

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,55 @@
 #![no_main]
 
+use png::ColorType;
+
 #[no_mangle]
-fn fake_img() -> usize {
-    // Image loading/saving is outside scope of this library
-    let width = 10;
-    let height = 10;
-    let fakebitmap = vec![imagequant::RGBA { r: 100, g: 200, b: 250, a: 255 }; width * height];
+fn deal_image(data: *const u8, length: usize) -> usize {
+    let slice = unsafe { std::slice::from_raw_parts(data, length) };
+    let mut image = png::Decoder::new(slice).read_info().unwrap();
+    let buffer_size = image.output_buffer_size();
+    let mut img_data = vec![0; buffer_size];
+    let image_info = image.next_frame(&mut img_data).unwrap();
+
+    let width = image_info.width as usize;
+    let height = image_info.height as usize;
 
     // Configure the library
     let mut liq = imagequant::new();
     liq.set_speed(5).unwrap();
     liq.set_quality(70, 99).unwrap();
 
+    let color_type = image_info.color_type;
+    // I only support RGBA and RGB now
+    let rgba = match color_type {
+        ColorType::Rgb => {
+            img_data
+                .chunks_exact_mut(3)
+                .map(|chunk| imagequant::RGBA {
+                    r: chunk[0],
+                    g: chunk[1],
+                    b: chunk[2],
+                    a: 0xFF,
+                })
+                .collect::<Vec<_>>()
+        }
+        ColorType::Rgba => {
+            img_data
+                .chunks_exact_mut(4)
+                .map(|chunk| imagequant::RGBA {
+                    r: chunk[0],
+                    g: chunk[1],
+                    b: chunk[2],
+                    a: chunk[3],
+                })
+                .collect::<Vec<_>>()
+        }
+        ColorType::Grayscale => { panic!("Grayscale is not supported yet.") }
+        ColorType::Indexed => { panic!("Indexed is not supported yet.") }
+        ColorType::GrayscaleAlpha => { panic!("GrayscaleAlpha is not supported yet.") }
+    };
+
     // Describe the bitmap
-    let mut img = liq.new_image(&fakebitmap[..], width, height, 0.0).unwrap();
+    let mut img = liq.new_image(rgba, width, height, 0.0).unwrap();
 
     // The magic happens in quantize()
     let mut res = match liq.quantize(&mut img) {
